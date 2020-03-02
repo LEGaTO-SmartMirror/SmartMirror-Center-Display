a93ee82685477a3a6aec4490022902f8c9c7c194
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2020-03-02 11:43:39 +0100

    Added cpp center display

--- node_helper.js
+++ node_helper.js
@@ -1,10 +1,10 @@
 'use strict';
 const NodeHelper = require('node_helper');
-
+const { spawn, exec } = require('child_process');
 const {PythonShell} = require('python-shell');
 var pythonStarted = false
 
-
+var cAppStarted = false
 //var websockets = require("websockets");
 
 module.exports = NodeHelper.create({
@@ -35,14 +35,51 @@ module.exports = NodeHelper.create({
 			}
     		});
 
+	},
+	cApp_start: function () {
+		const self = this;
+		self.centerDisplay = spawn('modules/' + this.name + '/cplusplus/build/center_display',['modules/' + this.name + '/build/center_display', self.config]);
+		self.centerDisplay.stdout.on('data', (data) => {
+			try{
+				var parsed_message = JSON.parse(`${data}`)
+
+				if (parsed_message.hasOwnProperty('DETECTED_OBJECTS')){
+					//console.log("[" + self.name + "] detected object: " + parsed_message.detected.name + " center in "  + parsed_message.detected.center);
+					self.sendSocketNotification('DETECTED_OBJECTS', parsed_message);
+				}else if (parsed_message.hasOwnProperty('OBJECT_DET_FPS')){
+					//console.log("[" + self.name + "] object detection fps: " + JSON.stringify(parsed_message));
+					self.sendSocketNotification('OBJECT_DET_FPS', parsed_message.OBJECT_DET_FPS);
+				}else if (parsed_message.hasOwnProperty('STATUS')){
+					console.log("[" + self.name + "] status received: " + JSON.stringify(parsed_message));
+				}
+			}
+			catch(err) {
+				
+				//console.log(err)
+			}
+  			//console.log(`stdout: ${data}`);
+		});	
+		exec(`renice -n 20 -p ${self.objectDet.pid}`,(error,stdout,stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+  				}
+			});
+
   	},
+	  
+
 
 	// Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
 	const self = this;
 	if(notification === 'CONFIG') {
       this.config = payload
-      this.python_start(); 
+	  this.python_start(); 
+	  if (!cAppStarted){
+		this.cApp_start();
+		cAppStarted = true;
+	  }  
+	  
     }else if(notification === 'CENTER_DISPLAY'){
 		var data = {"SET": payload};
 		self.pyshell.send(JSON.stringify(data));
